e55779e46c25a21cd6d9e04d4cd157125103d542
DANG PHAM <dangph2011@users.noreply.github.com>
2020-02-19 14:59:16 +0700

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -3,9 +3,6 @@
 #dependencies
 apt-get install zsh curl git -y
 
-#change shell default
-chsh -s $(which zsh)
-
 #install zsh by using curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
 
