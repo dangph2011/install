cb6570141f82fa5b9fa2298166ad65de401c844f
DANG PHAM <dangph2011@users.noreply.github.com>
2020-02-19 14:23:52 +0700

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -9,17 +9,24 @@ chsh -s $(which zsh)
 #install zsh by using curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
 
+#install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 #add zsh-syntax-highlighting plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 #add zsh-autosuggestions plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+#add fzf-tab-completion plugin
+git clone https://github.com/lincheney/fzf-tab-completion ~/.oh-my-zsh/custom/plugins/fzf-tab-completion
 
 #backup zshrc
 cp ~/.zshrc ~/.zshrc.bk
 
 #enable plugin in zshrc
-sed -i -e 's/plugins=(git/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fzf/g' ~/.zshrc
+sed -i -e 's/plugins=(git/plugins=(git zsh-autosuggestions zsh-syntax-highlighting fzf fzf-tab-completion/g' ~/.zshrc
 
 #append bindkey
 echo "bindkey '^ ' autosuggest-accept" >> ~/.zshrc
