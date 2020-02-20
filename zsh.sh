efc11356bd13ed1ff7a576ce98e8df4867ca3630
DANG PHAM <dangph2011@users.noreply.github.com>
2020-02-20 09:33:47 +0700

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #dependencies
-apt-get install zsh curl git -y
+sudo apt-get install zsh curl git -y
 
 #change shell default
 chsh -s $(which zsh)
 
 #install zsh by using curl
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
 
 #install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
