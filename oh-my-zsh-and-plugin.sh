1e4f5e966ad50fca365ba0dc486b7145cb8acee1
DANG PHAM <dangph2011@users.noreply.github.com>
2020-02-20 10:32:40 +0700

    Update oh-my-zsh-and-plugin.sh

--- oh-my-zsh-and-plugin.sh
+++ oh-my-zsh-and-plugin.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#install zsh by using curl
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
-
 #install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
