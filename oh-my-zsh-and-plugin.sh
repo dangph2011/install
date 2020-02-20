fb4b177668f428ad92a2e58773a56e660fb45d56
DANG PHAM <dangph2011@users.noreply.github.com>
2020-02-20 09:42:47 +0700

    Update and rename plugin.sh to oh-my-zsh-and-plugin.sh

--- oh-my-zsh-and-plugin.sh
+++ oh-my-zsh-and-plugin.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#install zsh by using curl
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" -y
+
 #install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
